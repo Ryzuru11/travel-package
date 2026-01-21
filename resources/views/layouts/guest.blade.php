c6ad34a1e688ae5abb12e48bf513d710dbcbb61f
Ryzuru11 <ryzuru11@gmail.com>
2026-01-21 20:29:47 +0700

    Update: Add full Laravel project

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,5 +1,5 @@
 {{-- in layouts folder, mainStructure file has user navigation bar and footer --}}
-@extends('layouts/mainStructure')
+@extends('layouts.guest')
 
 @section('content')
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
